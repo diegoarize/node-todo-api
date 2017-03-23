dc1aafafb61de641b4e7a5c746cd66e279cb4b44
diegoarize <diegoarize@outlook.com>
2017-03-22 21:40:58 -0300

    adding new routes and tests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -22,6 +23,34 @@ app.post('/todos', (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({
+        todos
+      })
+    },
+    (e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo})
+    })
+    .catch((e) => res.status(400).send())
+
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: "First test todo",
+  _id: "58d316af3b28d2801f60a6ba"
+}, {
+  text: 'second test todo'
+}]
+
 beforeEach(function (done) {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  })
+    .then(() => done());
 });
 
 describe('POST /todos', function () {
@@ -24,7 +34,7 @@ describe('POST /todos', function () {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -42,9 +52,54 @@ describe('POST /todos', function () {
           return done(err);
         }
         Todo.find().then(todos => {
-          expect(todos.length === 0);
+          expect(todos.length === 2);
           done();
         }).catch(err => done(err));
       });
+  });
+});
+
+describe('GET /todos', function () {
+  it('should get all todos', function (done) {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
+
+
+describe('GET /todos/:id', function () {
+  it('should return an emty body and status 404 for an invalid id', function (done) {
+    request(app)
+      .get('/todos/1g2g1h')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+
+  it('should return a valid todo when id matches', function (done) {
+    request(app)
+    .get('/todos/58d316af3b28d2801f60a6ba')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toEqual("First test todo");
+    })
+    .end(done);
+  });
+
+  it('Should return 404 and empty body when todo not found', (done) => {
+    request(app)
+    .get('/todos/48d316af3b28d2801f60a6ba')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toEqual({});
+    })
+    .end(done);
   });
 });
