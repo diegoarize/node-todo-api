9bddf7fe77ca5eb8e6f31bd36022140fc7c9493a
diegoarize <diegoarize@outlook.com>
2017-04-02 21:38:44 -0300

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -296,3 +296,23 @@ describe('POST /users/login', function () {
       });
   });
 });
+
+describe('DELETE /users/me/token', function () {
+  it('should remove auth token on logout', function (done) {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          done(err);
+        }
+
+        User.findOne({email: users[0].email})
+          .then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch(err => done(err));
+      });
+  });
+});
